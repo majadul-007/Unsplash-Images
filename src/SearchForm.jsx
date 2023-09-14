a56d212d7e7bd012feae1b39bb75ec4ca9ecf5e5
majadul-007 <tusarhaque12@gmail.com>
2023-09-14 14:17:06 +0600

    first commit

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -20,7 +20,7 @@ const SearchFrom = () => {
     <section>
       <h1 className='title'>Unsplash Images</h1>
       <form className="search-form" onSubmit={handleSubmit}>
-        <input type="text" className="form-input search-input" name='search' placeholder='Search Here..' />
+        <input type="text" className="form-input search-input" name='search' placeholder='Siberia..' />
         <button type='submit' className='btn'>
           Search
         </button>
